3a215dfbcd92ccd42a1d0b2a3508b3c097d70e9f
Matheus Santos <maatheus.santos223@gmail.com>
2024-04-15 23:22:15 -0300

    feat: change postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesControllersModule } from './presentation/controllers/movies/movies-controllers.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersControllersModule } from './presentation/controllers/users/users-controllers.module';
-import { PORT, DB, DB_PASSWORD, DB_USERNAME } from './env';
-
 @Module({
   imports: [
     MoviesControllersModule,
@@ -13,10 +11,7 @@ import { PORT, DB, DB_PASSWORD, DB_USERNAME } from './env';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'postgres',
-      port: PORT,
-      username: DB_USERNAME,
-      password: DB_PASSWORD,
-      database: DB,
+      url: `${process.env.DATABASE_URL}`,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
       synchronize: false,
       migrations: [`${__dirname}/infra/database/migrations/{.ts,*.js}`],
